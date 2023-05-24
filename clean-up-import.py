5b6d6ae9a96ab486194f8b737bfd359c84b5c49a
Anthony Esau <anthony.esau@gmail.com>
2023-05-24 17:32:51 -0500

    add vw image name cleaner

--- clean-up-import.py
+++ clean-up-import.py
@@ -7,7 +7,7 @@ from bpy.props import StringProperty, BoolProperty
 
 bl_info = {
    "name": "Clean Up Import",
-   "version": (0, 5),
+   "version": (0, 6),
    "blender": (3, 5, 0)
 }
 
@@ -26,6 +26,8 @@ class CustomPanel(bpy.types.Panel):
         row.operator("custom.delete_camera", text="Delete Camera(s) 'CINEMA_4D_Editor'")
         row = layout.row()
         row.operator("custom.delete_def_vw_lights", text="Delete default lights from VW")
+        row = layout.row()
+        row.operator("custom.clean_vw_img_names", text="Clean VW image names")
         row = layout.row()
         row.operator("rempack.open_filebrowser", text="Remove Pack then Link")
         
@@ -97,6 +99,44 @@ class DeleteVWLights(bpy.types.Operator):
         
         return {'FINISHED'}
 
+class CleanVWImageNames(bpy.types.Operator):
+    bl_idname = "custom.clean_vw_img_names"
+    bl_label = "Clean up Vectorworks Image Names."
+    bl_description = "Clean up Vectorworks Image Names."
+
+    # ----- Clean up Vectorworks Image Names -----
+
+    # TODO: 
+
+    def execute(self, context):
+
+        print("----- Clean up Vectorworks image names -----")
+
+        # Create a list of prefixes to clean up
+        vw_tex_substrings = ["NNA#3_", "NNA#2_", " Color", ".001"]
+
+        for image in bpy.data.images:
+
+            # replace instances of the substrings in image file names and paths
+            for vw_tex_substring in vw_tex_substrings:
+                
+                # changing filepath_raw and filepath are not ultimately needed
+                # so they are not run in case the replace command goes awry on the full file path
+                
+                # print(image.filepath_raw)
+                # image.filepath_raw = image.filepath_raw.replace(vw_tex_substring, "")
+                # print(image.filepath_raw)
+                
+                # print(image.filepath)
+                # image.filepath = image.filepath.replace(vw_tex_substring, "")
+                # print(image.filepath)
+                
+                print(image.name)
+                image.name = image.name.replace(vw_tex_substring, "")
+                print(image.name)
+        
+        return {'FINISHED'}
+
 
 class RemovePack_OpenFilebrowser(Operator, ImportHelper): 
     bl_idname = "rempack.open_filebrowser" 
@@ -139,13 +179,15 @@ def register():
     bpy.utils.register_class(MergeDuplicateTexturesOperator)
     bpy.utils.register_class(DeleteCameraOperator)
     bpy.utils.register_class(DeleteVWLights)
+    bpy.utils.register_class(CleanVWImageNames) 
     bpy.utils.register_class(RemovePack_OpenFilebrowser) 
 
 def unregister():
     bpy.utils.unregister_class(CustomPanel)
     bpy.utils.unregister_class(MergeDuplicateTexturesOperator)
     bpy.utils.unregister_class(DeleteCameraOperator)
-    bpy.utils.register_class(DeleteVWLights)
+    bpy.utils.unregister_class(DeleteVWLights)
+    bpy.utils.unregister_class(CleanVWImageNames) 
     bpy.utils.unregister_class(RemovePack_OpenFilebrowser) 
 
 if __name__ == "__main__":
